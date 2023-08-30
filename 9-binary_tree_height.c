85636ae6f23f110967c43f5c6cec1133b6f03740
mina-william-23 <capeandroid50@gmail.com>
2023-08-30 12:58:36 -0700

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - compute height of tree
+ * @tree: binary tree pointer
+ * Return: height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+	if (left > right)
+		return (left);
+	else
+		return (right);
+
+}
