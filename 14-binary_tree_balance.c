232aa5ce6346c42bf7fa3a02a41adaa7e2926d66
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-08-31 00:07:20 +0300

    task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: the binary tree
+ * Return: balance factor
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left, right;
+
+	if (!tree)
+	return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
+}
