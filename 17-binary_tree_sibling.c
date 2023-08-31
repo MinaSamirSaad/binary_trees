6c858faa3c1b9fd11102e73dc523cb1e3176725e
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-01 00:07:58 +0300

    task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling -  finds the sibling of a node
+ * @node: the node of binary tree
+ * Return: the sibling of node
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+    if(!node || !node->parent)
+    return(NULL);
+
+    if(node->parent->left == node)
+    return (node->parent->right);
+    else
+    return (node->parent->left);
+}
