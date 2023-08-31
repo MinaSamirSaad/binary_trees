97851893dae30a167316090d0ed345641cbc015a
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-01 00:09:03 +0300

    task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,11 +6,11 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if(!node || !node->parent)
-    return(NULL);
+	if (!node || !node->parent)
+	return (NULL);
 
-    if(node->parent->left == node)
-    return (node->parent->right);
-    else
-    return (node->parent->left);
+	if (node->parent->left == node)
+	return (node->parent->right);
+	else
+	return (node->parent->left);
 }
