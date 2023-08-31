fcddfdacf205d59297f3a5915e53e49db527090e
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-08-31 22:44:21 +0300

    task 14

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,14 +12,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		left = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		right = 1 + binary_tree_height(tree->right);
-
+	left += binary_tree_height(tree->left);
+	right  += binary_tree_height(tree->right);
+/*1 for parent*/
 	if (left > right)
-		return (left);
+		return (left + 1);
 	else
-		return (right);
-
+		return (right + 1);
 }
