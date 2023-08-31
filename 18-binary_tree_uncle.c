5722a582797a700113774d60c98f1fcfe43c832e
MinaSamirSaad <minasamir.nashid@gmail.com>
2023-09-01 00:13:09 +0300

    task 18

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_uncle -  finds the uncle of a node
+ * @node: the node of binary tree
+ * Return: the uncle of node
+*/
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+	return (NULL);
+
+	if (node->parent->parent->left == node->parent)
+	return (node->parent->parent->right);
+	else
+	return (node->parent->parent->left);
+}
